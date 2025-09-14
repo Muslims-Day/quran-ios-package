5352f6217630425628bf9caf923befcf3458396a
shafayat <shafayathossainkhan@gmail.com>
2025-09-15 04:17:05 +0600

    Corrected binary target url

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KahfQuranSdk",
-            url: "https://github.com/Muslims-Day/quran-ios-package/releases/download/v1.1/KahfQuranSdk.xcframework.zip",
+            url: "https://github.com/Muslims-Day/quran-ios-package/releases/download/v1.1.0/KahfQuranSdk.xcframework.zip",
             checksum: "fbd87fe03c373d8944469bc0b5a154eb55ef0e83f0676b68c24eea6d1ec5aff0"
         )
     ]
