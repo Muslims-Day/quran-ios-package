949e2cf0b20c64c1acee3160a43d49e51721828d
shafayat <shafayathossainkhan@gmail.com>
2025-09-29 06:22:57 +0600

    Updated framework file

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "KahfQuranSdk",
             url: "https://github.com/Muslims-Day/quran-ios-package/releases/download/v1.1.1/KahfQuranSdk.xcframework.zip",
-            checksum: "b1b51b930185057ab938b991e7c4ebb9905a57eceb3140c266b8291cbedc17d9"
+            checksum: "c5158e8fc8518ba4b9a1216745b36e84fbd64f38241b9520fb9a671dda1e4563"
         )
     ]
 )
